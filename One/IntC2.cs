77eb4df3aa07250e73b21c37649d48cf505611a9
digitalcanopus <rocky-cat@mail.ru>
2022-10-26 22:27:01 +0300

    dll for insertion

---
+++ One/IntC2.cs
@@ -0,0 +1,17 @@
+﻿using Interfaces;
+
+namespace One
+{
+    public class IntC2 : ICreate
+    {
+        public object Create(Type type)
+        {
+            Random rnd = new Random();
+            return rnd.Next(int.MinValue, int.MaxValue);
+        }
+        public bool Exist(Type type)
+        {
+            return type == typeof(int);
+        }
+    }
+}
---
+++ Two/StringC2.cs
@@ -0,0 +1,28 @@
+﻿using Interfaces;
+
+using System.Text;
+
+namespace Two
+{
+    public class StringC2 : ICreate
+    {
+        public string Letters { get; set; } = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+        public int MinLen { get; set; } = 5;
+        public int MaxLen { get; set; } = 20;
+
+        public object Create(Type type)
+        {
+            Random rnd = new Random();
+            int length = rnd.Next(MinLen, MaxLen + 1);
+            var str = new StringBuilder(length);
+            for (int i = 0; i < length; i++)
+                str.Append(Letters[rnd.Next(Letters.Length)]);
+            return str.ToString();
+        }
+
+        public bool Exist(Type type)
+        {
+            return type == typeof(string);
+        }
+    }
+}
